61643adc1ec568d53220b691b199d2b7e9a11191
Rosseleen Nunez <nrosseleen@gmail.com>
2022-06-10 23:46:08 -0400

    completed remove method

--- Part B/js/BinarySearchTree.js
+++ Part B/js/BinarySearchTree.js
@@ -153,7 +153,184 @@ export default class BinarySearchTree {
 
     // @todo - YOU MUST DEFINE THIS METHOD
     removeValue(key) {
+        if (this.root != null) {
+            let compare = key.localeCompare(this.root.key);
+            if (compare == 0) { // 3 CASES: 0 CHILDREN, 1 CHILD, 2 CHILDREN:
+                if ((this.root.left == null) && (this.root.right == null)) { // do nothing, root node has been deleted
+                    this.root = null;
+                }
+                else if ((this.root.left != null) && (this.root.right != null)) {
+                    // two children
+                    let node = root.right;
+                    if (node.left != null) {
+                        let node = findMinRight(this.root); // node to replace root with
+                        this.root.data = node.data;
+                        this.root.key = node.key;
+                        if (node.right != null) // if node that is replaced has a right branch(connect it to previous node)
+                        {
+                            let befNode = findPrevMinRight(this.root); // get node before replaced node
+                            befNode.left = node.right;            // reconnect right branch from replaced node with the node before it
 
+                            node = null;
+                            this.size--;
+                        }
+                        else {
+                            let befNode = findPrevMinRight(this.root); // get the node before the replaced node
+                            befNode.left = null;                // disconnect the branch on the left side
+                            node = null;
+                            this.size--;
+                        }
+                    }
+                    else {
+                        this.root.data = node.data;
+                        this.root.key = node.key;
+                        this.root.right = node.right;
+                        this.size--;
+                    }
+                }
+                else if ((this.root.left != null) || (this.root.right != null)) { // 1 child
+                    if (this.root.left != null) {
+                        this.root = this.root.left;
+                    }
+                    else {
+                        this.root = this.root.right;
+                    }
+                }
+            }
+            else {
+                let preNode = this.root;
+                let node = this.root;
+                let movedLeft = 0; // 1 = left; 0 = right
+                if (node != null) compare = key.localeCompare(node.key);
+                if (compare > 0) {
+                    node = node.left;
+                    movedLeft = 1;
+                }
+                else {
+                    node = node.right;
+                    movedLeft = 0;
+                }
+                if (node != null) compare = key.localeCompare(node.key);
+                while (node != null) // search through tree until we find the node to remove
+                {
+                    if (compare == 0) // key found now we remove da node
+                    {
+                        if ((node.left != null) && (node.right != null)) // two children
+                        {                                                        // two children
+                            let newNode = node.right;                         // get the right node of the node to  be deleted
+                            if (newNode.left != null)                        // check if the right node of the TBD node exists(so we can find replaced node)
+                            {
+                                let rNode = findMinRight(node); // node to replace root with (rNode)
+                                node.data = rNode.data;
+                                node.key = rNode.key;      // replace the deleted node data with the new replaced node data
+                                if (rNode.right != null) // if node that is replaced has a right branch(connect it to previous node)
+                                {
+                                    let befNode = findPrevMinRight(node); // get node before replaced node
+                                    befNode.left = rNode.right;           // reconnect right branch from replaced node with the node before it
+
+                                    if (movedLeft) {
+                                        preNode.left = node;
+                                    }
+                                    else {
+                                        preNode.right = node;
+                                    }
+                                }
+                                else {
+                                    let befNode = findPrevMinRight(node); // get the node before the replaced node
+                                    befNode.left = null;                // disconnect the branch on the left side
+                                }
+                                node = null;
+                                this.size--;
+                            }
+                            else {
+                                newNode.left = node.left;
+                                if (movedLeft) {
+                                    preNode.left = newNode;
+                                }
+                                else {
+                                    preNode.right = newNode;
+                                }
+                                node = null;
+                                this.size--;
+                            }
+                        }
+                        else if ((node.left != null) || (node.right != null)) // 1 child
+                        {
+                            if (movedLeft) {
+                                if (node.left != null) {
+                                    preNode.left = node.left;
+                                }
+                                else {
+                                    preNode.left = node.right;
+                                }
+                            }
+                            else {
+                                if (node.left != null) {
+                                    preNode.right = node.left;
+                                }
+                                else {
+                                    preNode.right = node.right;
+                                }
+                            }
+                            node = null;
+                            this.size--;
+                        }
+                        else // no children
+                        {
+                            if (movedLeft) {
+                                preNode.left = null;
+                                node = null;
+                                this.size--;
+                            }
+                            else {
+                                preNode.right = null;
+                                node = null;
+                                this.size--;
+                            }
+                        }
+                    }
+                    else if (compare < 0) // key is bigger than node(MOVE RIGHT)
+                    {
+                        preNode = node;
+                        node = node.right;
+                        movedLeft = 0;
+                        if (node != null) compare = key.localeCompare(node.key);
+                    }
+                    else if (compare > 0) // key is smaller than node(MOVE LEFT
+                    {
+                        preNode = node;
+                        node = node.left;
+                        movedLeft = 1;
+                        if (node != null) compare = key.localeCompare(node.key);
+                    }
+                }
+            }
+        }
+
+    }
+
+    findMinRight(node) // Find min node of the right branch of the given root/node
+    {
+        let prevNode = node.right;
+        let node1 = node.right;
+        while (node1 != null) {
+            prevNode = node1;
+            node1 = node1.left;
+        }
+        return prevNode;
+    }
+
+    findPrevMinRight(node) // Find min node of the right branch of the given root/node
+    {
+        let ppreNode = node.right;
+        let prevNode = node.right;
+        let node1 = node.right;
+        while (node1 != null) {
+            ppreNode = prevNode;
+            prevNode = node1;
+            node1 = node1.left;
+        }
+        return ppreNode;
     }
 
     toStringRecursively(traveller, level) {
